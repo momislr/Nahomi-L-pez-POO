537d1d82da98e2bbbb374b5e23e117680c5de3ae
momislr <rojaszujayli@gmail.com>
2026-04-09 14:01:35 -0600

    Create PracDos.cs

---
+++ PracDos.cs
@@ -0,0 +1,16 @@
+using System; 
+
+public class Program
+{
+    static public void Main (String []args)
+    {
+		
+	
+	 for(int numero= 0; numero <= 10; numero++){
+	 	for(int i = 0; i <=10; i++){
+	 
+	 	Console.WriteLine(numero+"x"+i+"="+(numero*i));
+		} 
+	 }
+  }
+}

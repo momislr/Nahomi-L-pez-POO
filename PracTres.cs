b1f9d3166392c2ee903b8008480b8ff6e575ecb8
momislr <rojaszujayli@gmail.com>
2026-04-09 14:02:19 -0600

    Create PracTres.cs

---
+++ PracTres.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Program
+{
+    static public void Main ()
+    {
+        for(int numero = 0; int numero <= 101; i++){
+			if(i % == 3);
+			Consola.WriteLine("Fizz");
+			if(i % == 5);
+			Consola.WriteLine("Buz");
+			if(i % 3);
+			Consola.WriteLine("FizzBuzz");
+		} 
+    }
+}

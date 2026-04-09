61038a2031d94497510b3f009f639aaf37ba3367
momislr <rojaszujayli@gmail.com>
2026-04-09 14:11:18 -0600

    Create PracCinco.cs

---
+++ PracCinco.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class HelloWorld
+{
+    static public void Main ()
+    {
+        Console.WriteLine ("Hello World");
+        
+		int edad = 21;
+		string nombre = "Momis";
+		float estatura = 1.70f;
+		double PI = 3.1416;
+		char caracter = 'N';
+		bool titulo = false;
+		Console.WriteLine("Mi nombre es: "+ nombre);
+		Console.WriteLine("Mi eda es: "+ edad);
+	  Console.WriteLine("Mi estatura es: "+ estatura);
+	  Console.WriteLine("La inicial de mi nombre es: "+ caracter); 
+	  Console.WriteLine("Conozco que la circunferencia del circulo es: "+ PI);	
+    }
+}
